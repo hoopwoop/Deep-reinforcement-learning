8792424354f6a25c4958556d8025f5ba1a9b1d3e
hoopwoop <kuanholao@gmail.com>
2017-03-06 14:13:44 +0800

    add gym environment test

---
+++ gym environment test/gym environment test.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  4 20:18:20 2017
+
+@author: Kuanho
+
+Environment description:
+Landing pad is always at coordinates (0,0). Coordinates are the first two numbers in state vector. 
+Reward for moving from the top of the screen to landing pad and zero speed is about 100..140 points. 
+If lander moves away from landing pad it loses reward back. 
+Episode finishes if the lander crashes or comes to rest, receiving additional -100 or +100 points.
+Each leg ground contact is +10. Firing main engine is -0.3 points each frame. 
+Solved is 200 points. Landing outside landing pad is possible. 
+Fuel is infinite, so an agent can learn to fly and then land on its first attempt. 
+Action is two real values vector from -1 to +1. 
+First controls main engine, -1..0 off, 0..+1 throttle from 50% to 100% power. 
+Engine can't work with less than 50% power. 
+Second value -1.0..-0.5 fire left engine, +0.5..+1.0 fire right engine, -0.5..0.5 off.
+"""
+
+
+import gym
+env = gym.make('LunarLanderContinuous-v2')
+print(env.action_space)
+print(env.observation_space)
+print(env.action_space.high)
+print(env.action_space.low)
+
+'''for i_episode in range(1):
+    observation = env.reset()
+    for t in range(1000):
+        env.render()
+        print(observation)
+        print(action)
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        
+        while (input("Press Enter to continue...")): print('wait')
+        if done:
+            print("Episode finished after {} timesteps".format(t+1))
+            break
+env.render(close=True)'''
